fc3a0c0f05d7d3502069d35e2e5202f6a1fc9146
SYED ISHAQ <229y1a05h4@ksrmce.ac.in>
2026-02-18 06:47:46 +0530

    Change route path for Home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
         <ToastContainer />
         <Navbar setShowLogin={setShowLogin} />
         <Routes>
-          <Route path="/Food-Delivery-Platform" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
           <Route path="/verify" element={<Verify />} />
